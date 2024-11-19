aaa6c4fd44dae73da41323665685a6a9e584cd06
Gusuyoung <suyoung606123@gmail.com>
2024-11-19 16:51:40 +0900

    11-19

---
+++ src/main/java/com/green/greengramver1/user/UserController.java
@@ -0,0 +1,16 @@
+package com.green.greengramver1.user;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("user")
+@Tag(name = "유저", description = "화원가입, 로그인, 마이페이지, 비밀번호 변경, 정보 수정 등 처리")
+public class UserController {
+    private final UserService service;
+}
