38e7ec9021b9c23d46a5576b92b115e28bbc4f1b
Gusuyoung <suyoung606123@gmail.com>
2024-11-20 10:54:42 +0900

    11-20(1)

--- src/main/java/com/green/greengramver1/user/model/UserInsReq.java
+++ src/main/java/com/green/greengramver1/user/model/UserInsReq.java
@@ -1,13 +1,18 @@
 package com.green.greengramver1.user.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UserInsReq {
+    @Schema(description = "유저 아이디", example = "mic", requiredMode = Schema.RequiredMode.REQUIRED)
     private String uid;
+    @Schema(description = "유저 비밀번호", example = "1212", requiredMode = Schema.RequiredMode.REQUIRED)
     private String upw;
+    @Schema(description = "유저 프로필 사진")
     private String pic;
+    @Schema(description = "유저 닉네임", example = "홍길동")
     private String nickName;
 }
